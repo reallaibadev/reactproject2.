41eded1d28a917906799b6546e73e881e279d4ea
reallaibadev <laibanadeem1218my@gmail.com>
2026-03-31 08:34:32 +0500

    Add files via upload
    react project 2

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NestedList from "./components/NestedList";
+
+export default function App() {
+  return (
+    <>
+      <div className="container">
+        <NestedList />
+      </div>
+    </>
+  );
+}
